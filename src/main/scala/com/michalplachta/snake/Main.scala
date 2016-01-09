2389ca1e9e1f3cf3bb88401a0de6a93bd8744fa4
miciek <miciek@gmail.com>
2016-01-09 17:40:09 +0100

    First implementation of Snake multiplayer stream: WebSocket and broadcasting

--- src/main/scala/com/michalplachta/snake/Main.scala
+++ src/main/scala/com/michalplachta/snake/Main.scala
@@ -1,21 +1,66 @@
 package com.michalplachta.snake
 
-import akka.actor.ActorSystem
+import akka.actor._
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.{ActorMaterializer, OverflowStrategy}
 
 object Main extends App {
   implicit val system = ActorSystem("snake")
   implicit val materializer = ActorMaterializer()
 
+  case class GameEvent(s: String)
+  case class PlayerJoined(actor: ActorRef)
+  case class PlayerLeft(name: String)
+
+  val actor =
+    system.actorOf(Props(new Actor {
+      var subscribers = Set.empty[ActorRef]
+
+      def receive: Receive = {
+        case GameEvent(s) =>
+          println("Received " + s)
+          subscribers.foreach(_ ! s)
+        case PlayerJoined(actor) =>
+          println("Joined " + actor)
+          context.watch(actor)
+          subscribers += actor
+        case Terminated(actor) =>
+          println("Terminated " + actor)
+          subscribers = subscribers.filterNot(_ == actor)
+        case msg =>
+          println("UNKNOWN: " + msg)
+      }
+    }))
+
+  val sink = Sink.actorRef(actor, "finished")
+
+  val broadcastFlow: Flow[String, String, _] = {
+    val in = Flow[String]
+      .map(GameEvent)
+      .to(sink)
+
+    val out =
+      Source.actorRef[String](1, OverflowStrategy.fail)
+        .mapMaterializedValue(actor ! PlayerJoined(_))
+
+    Flow.fromSinkAndSource(in, out)
+  }
+
   val flow: Flow[Message, Message, _] = {
-    Flow[Message].collect {
-      case msg: TextMessage.Strict => msg
-    }
+    Flow[Message]
+      .collect {
+        case msg: TextMessage.Strict => msg.text
+      }
+      .via(broadcastFlow)
+      .map {
+        case msg: String =>
+          TextMessage.Strict(msg)
+      }
+      .via(debug)
   }
 
   val route: Route =
@@ -26,4 +71,10 @@ object Main extends App {
     }
 
   val serverBinding = Http().bindAndHandle(interface = "0.0.0.0", port = 8080, handler = route)
+
+  private def debug[T]: Flow[T, T, _] =
+    Flow[T].map { in =>
+      println("element: " + in)
+      in
+    }
 }
